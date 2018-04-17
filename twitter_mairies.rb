bb07e4ae8588b0a82dc9ab3718718d733a5c81ad
Thomas Szadek <thomasszadek@gmail.com>
2018-04-17 19:59:49 +0200

    mairies.rb 19h59 thomas

--- twitter_mairies.rb
+++ twitter_mairies.rb
@@ -5,7 +5,7 @@ require 'google_drive'
 require 'json'
 require 'csv'
 
-
+#Ce programme créer une nouvelle colonne
 
 def get_twitter_handle
   session = GoogleDrive::Session.from_config("config.json")
@@ -21,7 +21,19 @@ def get_twitter_handle
   twitter_link_modified = twitter_link.map{|element| element.text}
   ws[row,4] = twitter_link_modified[0]
   puts ws[row,4]
+
+  retry_attempts = 0
+  begin
   ws.save
+  rescue StandardError => e
+  retry_attempts +=1
+      if retry_attempts < 10
+        retry
+      else puts "Erreure fatale"
+      end
   end
+
+   end
 end
+
 get_twitter_handle
