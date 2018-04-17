13aef94f1dcfe041f488cbca848fedabbb9d1b8d
larrygold <larry@goldstein.fr>
2018-04-17 19:50:02 +0200

    mairies.rb

--- mairies.rb
+++ mairies.rb
@@ -19,26 +19,37 @@ end
 
 
 
-def get_all_the_urls_of_val_doise_townhalls
+def get_all_the_urls_of_all_townhalls(url_department)
 
 
-    page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+    page = Nokogiri::HTML(open(url_department))
 
-    links = page.css('.Style20 a')
-
+    links = page.css('a.lientxt')
+    puts links
     array_of_hashes = Array.new
 
     j = 0
 
     links.each do |i|
       page_url = i['href']
+      department = page_url[2] + page_url[3]
+      puts department
       page_url[0] = ""
       url = "http://annuaire-des-mairies.com" + page_url
       nom_ville = i.text
 
+      puts url
+
       url_hash = Hash.new
       url_hash[:name] = nom_ville
-      url_hash[:email] = get_the_email_of_a_townhal_from_its_webpage(url)
+      url_hash[:department] = department
+
+      begin
+        url_hash[:email] = get_the_email_of_a_townhal_from_its_webpage(url)
+      rescue OpenURI::HTTPError => e
+        url_hash[:email] = "unknown"
+      end
+
       array_of_hashes[j] = url_hash
 
       j +=1
@@ -54,61 +65,87 @@ end
 
 def drive
 
-list = get_all_the_urls_of_val_doise_townhalls
+url_all_departments = ["http://www.annuaire-des-mairies.com/vendee.html", "http://www.annuaire-des-mairies.com/bas-rhin.html", "http://www.annuaire-des-mairies.com/bas-rhin-2.html", "http://www.annuaire-des-mairies.com/cotes-d-armor.html", "http://www.annuaire-des-mairies.com/cotes-d-armor-2.html"]
 
 session = GoogleDrive::Session.from_config("config.json")
 
-ws = session.spreadsheet_by_key("18t9vrvd1oxsje3o2farAidH8h7KIWqZosqizRY9LbJo").worksheets[0]
-
+ws = session.spreadsheet_by_key("1Z_MDkn11Lym6HJpoGzmLp1aeGJmso2V0dpOkFkAHBuc").worksheets[0]
 
 ws[1, 1] = "Ville"
 ws[1, 2] = "Email"
+ws[1, 3] = "Département"
 
-nb_emails = get_all_the_urls_of_val_doise_townhalls.length
-(2..nb_emails+1).each do |row|
+row_nb = 2
+
+url_all_departments.each do |url_of_a_department|
+
+list = get_all_the_urls_of_all_townhalls(url_of_a_department)
+
+
+nb_emails = list.length
+
+arr_index = 0
+
+(row_nb..row_nb+nb_emails-1).each do |row|
     puts row
-    ws[row, 1] = list[row-2][:name]
+    ws[row, 1] = list[arr_index][:name]
     puts ws[row, 1]
-    ws[row, 2] = list[row-2][:email]
+    ws[row, 2] = list[arr_index][:email]
     puts ws[row, 2]
-
-end
-
-ws.save
-ws.reload
-
-end
-
-
-
-
-def json
-
-list = get_all_the_urls_of_val_doise_townhalls
-
-File.open("temp.json", "w") do |f|
-  f.write(list.to_json)
-end
-
-end
-
-
-
-
-def csv
-
-list = get_all_the_urls_of_val_doise_townhalls
-
-CSV.open("test.csv", "wb") do |line|
-
-list.each do |hash|
-line << hash.values
+    ws[row, 3] = list[arr_index][:department]
+    puts ws[row, 2]
+    arr_index += 1
     end
 
+retry_attempts = 0
+begin
+ws.save
+rescue StandardError => e
+  retry_attempts += 1
+  puts "Error: retrying #{retry_attempts} time(s) to load the code"
+  if retry_attempts < 10
+    retry
+  else puts "We retried 10 times, it didn't work out. Life sucks!"
   end
+end
+
+row_nb += nb_emails
 
 end
 
+
+
+end
+
+
+
+#
+# def json
+#
+# list = get_all_the_urls_of_val_doise_townhalls
+#
+# File.open("temp.json", "w") do |f|
+#   f.write(list.to_json)
+# end
+#
+# end
+#
+#
+#
+#
+# def csv
+#
+# list = get_all_the_urls_of_val_doise_townhalls
+#
+# CSV.open("test.csv", "wb") do |line|
+#
+# list.each do |hash|
+# line << hash.values
+#     end
+#
+#   end
+#
+# end
 
 
 
