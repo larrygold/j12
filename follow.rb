eaef11cc2c65544ca6a8a74224c4146bafcd73e8
MartaMcFly <37927133+MartaMcFly@users.noreply.github.com>
2018-04-18 15:23:20 +0200

    Update follow.rb

--- follow.rb
+++ follow.rb
@@ -13,10 +13,10 @@ def follow_users
   ws = session.spreadsheet_by_key("1Z_MDkn11Lym6HJpoGzmLp1aeGJmso2V0dpOkFkAHBuc").worksheets[0]
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "S4Slbwgfu2GDIQqKcWinjHroA"
-  config.consumer_secret     = "4kBE46p43Zx1A2vp5xHVp3IIglN7SvwdUu5VaynojP177Rhh7y"
-  config.access_token        = "986181539435286528-DGoX5tnMz1dJgZ26YQn9rJ3w2Mdloif"
-  config.access_token_secret = "0pcNdFCVpdTz3hOGE8fRbYVujBEWJobOPRiPb8iL8GEIV"
+  config.consumer_key        = ""
+  config.consumer_secret     = ""
+  config.access_token        = ""
+  config.access_token_secret = ""
 
                                   end
 (2..ws.num_rows).each do |row|
